b1475eb1f0c0d845a4005c8ef1dda9669f1841f5
dzelge <helge.dzierzon@brockmann-consult.de>
2021-05-20 09:23:50 +0200

    Fixed copying a non existent env file in install_cate.sh

--- scripts/install_cate.sh
+++ scripts/install_cate.sh
@@ -9,7 +9,6 @@ if [[ $CATE_INSTALL_MODE == "branch" ]]; then
   git clone https://github.com/CCI-Tools/cate
   cd cate || exit
   git checkout "${CATE_VERSION}"
-  cp /tmp/environment.yml .
 
   mamba env create . || exit
   source activate cate-env
@@ -22,7 +21,7 @@ elif [[ $CATE_INSTALL_MODE == "github" ]]; then
   tar xvzf v"${CATE_VERSION}".tar.gz
 
   cd cate-"${CATE_VERSION}" || exit
-  cp /tmp/environment.yml .
+
   mamba env create . || exit
   source activate cate-env
   python setup.py install
