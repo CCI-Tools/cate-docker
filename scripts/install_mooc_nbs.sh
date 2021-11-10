5a606d88b17b5be92d122e9894b1e05394ffb9c7
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2021-11-10 10:08:44 +0100

    fixed command

--- scripts/install_mooc_nbs.sh
+++ scripts/install_mooc_nbs.sh
@@ -9,7 +9,7 @@ git clone https://github.com/CCI-Tools/cate-edu
 cd cate-edu || exit
 git checkout alicja-xxx-update_of_nbs
 
-COPY --chown=1000:100 ECVs/ /home/jovyan/examples
+cp ECVs/ /home/jovyan/examples
 
 cd ..
 rm -rf cate-edu
